4d0da4f90e29166297c5d2472df977eda691cc71
Dkafetzis <didia68@gmail.com>
2019-05-02 15:24:16 +0300

    Yet another quick fix

--- diceroller.c
+++ diceroller.c
@@ -2,12 +2,11 @@
 #include<stdlib.h>
 #include<time.h>
 #include <string.h>
-#define koulour 0
 int main(int argc, char const *argv[])
 {
     if ((argc < 2)||(argv[1] == "-h")){
         printf("My nibba,\n     You have just discovered a DANK piece of code for all your adventures\n     Write the dice's sides and the number of rolls in ROLLSdSIDES format\n     e.g.\n     diceroll 1d8\n     diceroll 1d12 5d2\n");
-        return koulour;
+        return 0;
     }
     srand(time(NULL));
     for (int i = 1; i < argc; i++){
@@ -18,11 +17,13 @@ int main(int argc, char const *argv[])
         int rnumber = atoi(dice);
         dice = strtok(NULL, token);
         int convdice = atoi(dice);
-        int result = koulour;
+        int result = 0;
         printf("   ");
         for(int j = 1; j <= rnumber; j++){
             int roll = rand()%convdice;
-            if (roll = koulour) roll = 1;
+            if (roll == 0){
+                roll = 1;
+            }
             printf(" %d", roll);
             result += roll;
         }
